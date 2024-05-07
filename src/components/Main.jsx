9255ae427f2a5852dab3c908af46901da2990e7e
hhurdev <hhurdev@gmail.com>
2024-05-07 18:50:44 +0300

    working with fetch still

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,55 +1,11 @@
 import { FlatList, View, StyleSheet } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Route, Routes } from 'react-router-native';
+
 import AppBar from './AppBar';
 import RepositoryItem from './RepositoryItem';
-import { Route, Routes } from 'react-router-native';
 import SignIn from './SignIn'
-
-const repositories = [
-  {
-    id: 'jaredpalmer.formik',
-    fullName: 'jaredpalmer/formik',
-    description: 'Build forms in React, without the tears',
-    language: 'TypeScript',
-    forksCount: 1589,
-    stargazersCount: 21553,
-    ratingAverage: 88,
-    reviewCount: 4,
-    ownerAvatarUrl: 'https://avatars2.githubusercontent.com/u/4060187?v=4',
-  },
-  {
-    id: 'rails.rails',
-    fullName: 'rails/rails',
-    description: 'Ruby on Rails',
-    language: 'Ruby',
-    forksCount: 18349,
-    stargazersCount: 45377,
-    ratingAverage: 100,
-    reviewCount: 2,
-    ownerAvatarUrl: 'https://avatars1.githubusercontent.com/u/4223?v=4',
-  },
-  {
-    id: 'django.django',
-    fullName: 'django/django',
-    description: 'The Web framework for perfectionists with deadlines.',
-    language: 'Python',
-    forksCount: 21015,
-    stargazersCount: 48496,
-    ratingAverage: 73,
-    reviewCount: 5,
-    ownerAvatarUrl: 'https://avatars2.githubusercontent.com/u/27804?v=4',
-  },
-  {
-    id: 'reduxjs.redux',
-    fullName: 'reduxjs/redux',
-    description: 'Predictable state container for JavaScript apps',
-    language: 'TypeScript',
-    forksCount: 13902,
-    stargazersCount: 52869,
-    ratingAverage: 0,
-    reviewCount: 0,
-    ownerAvatarUrl: 'https://avatars3.githubusercontent.com/u/13142323?v=4',
-  },
-];
+import useRepositories from '../hooks/useRepositories';
 
 const styles = StyleSheet.create({
   separator: {
@@ -58,12 +14,21 @@ const styles = StyleSheet.create({
 });
 
 const repoList = () => {
+  const { repositories } = useRepositories();
+
+  // Get the nodes from the edges array
+  const repositoryNodes = repositories
+    ? repositories.edges.map(edge => edge.node)
+    : [];
+
+
   return (
     <FlatList
-        data={repositories}
+        data={repositoryNodes}
         renderItem={({item}) => <RepositoryItem item={item} />}
         ItemSeparatorComponent={ItemSeparator}
         keyExtractor={item => item.id}
+        style={{ flex: 1 }}
         // other props
       />
   )
@@ -77,7 +42,7 @@ const onSubmit = () => {
 
 const RepositoryList = () => {
   return (
-    <View style={{ backgroundColor: '#e1e4e8' }}>
+    <View style={{ flex: 1, backgroundColor: '#e1e4e8' }}>
       <AppBar></AppBar>
       <Routes>
         <Route path='/' element={repoList()} />
